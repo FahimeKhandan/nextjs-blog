80945a2fef040de81d5cdfcf7bb5b727fd639337
fahimekhandan <fahimekhandan@gmail.com>
2023-04-08 18:15:53 +0330

    add glob and read all md files in posts dir

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,44 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import path from 'path';
+import { getSortedPostsData } from '../lib/posts';
+import glob from 'glob'
 
-export default function Home() {
+export async function getStaticProps() {
+const postsDirectory = path.join(process.cwd(), 'posts');
+const mdFiles = await glob(path.join(postsDirectory, "**/*.md"))
+
+  const allPostsData = getSortedPostsData(mdFiles);
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+      {/* Keep the existing code here */}
+
+      {/* Add this <section> tag below the existing <section> tag */}
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title, fileContents }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {/* {title}
+              <br />
+              {id}
+              <br />
+              {date} */}
+              <br/>
+              {fileContents}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
-  )
+  );
 }
